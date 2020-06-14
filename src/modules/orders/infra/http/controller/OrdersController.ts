10cab5645a8bf12ffb4821c92bc961aa655bb9a2
pablosilva <pablo.silva.rodrigo@gmail.com>
2020-06-14 16:16:58 -0400

    [NodeJS][Challenge09][backend][ProductsEntity] - Add list of orders

--- src/modules/orders/infra/http/controller/OrdersController.ts
+++ src/modules/orders/infra/http/controller/OrdersController.ts
@@ -6,7 +6,15 @@ import CreateOrderService from '@modules/orders/services/CreateOrderService';
 import FindOrderService from '@modules/orders/services/FindOrderService';
 
 export default class OrdersController {
-  public async show(request: Request, response: Response): Promise<Response> {}
+  public async show(request: Request, response: Response): Promise<Response> {
+    const order_id = request.params.id;
+
+    const showOrder = container.resolve(FindOrderService);
+
+    const order = await showOrder.execute({ id: order_id });
+
+    return response.json(order);
+  }
 
   public async create(request: Request, response: Response): Promise<Response> {
     const { customer_id, products } = request.body;
